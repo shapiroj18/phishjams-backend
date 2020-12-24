10d9179e5945316cfa21a817413ffae344fe9f46
Jonathan Shapiro <shapiroj18@gmail.com>
2020-12-24 12:46:19 -0500

    added shell context processor

--- phish_bot.py
+++ phish_bot.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import Subscribers
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"db": db, "Subscribers": Subscribers}
