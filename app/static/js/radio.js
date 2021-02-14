cfde9b509b12d3243de37c36860336b1e0fa110c
Jonathan Shapiro <shapiroj18@gmail.com>
2021-02-13 19:45:52 -0500

    removing unnecessary array

--- app/static/js/radio.js
+++ app/static/js/radio.js
@@ -1,21 +1,3 @@
-const songs = [
-    {
-    "name": "Shafty",
-    "artist": "Phish",
-    "url": "https://phish.in/audio/000/018/032/18032.mp3",
-    "cover_art_url": "static/img/livephish_logos/1999-12-31.jpg"
-    },
-    {
-      "name": "Harry Hood",
-      "artist": "Phish",
-      "url": "https://phish.in/audio/000/020/602/20602.mp3",
-      "cover_art_url": "static/img/livephish_logos/1999-12-31.jpg"
-      }
-]
-
-console.log(typeof songs)
-
-
 const jsonResp = fetch("/get_song_info")
   .then((response) => response.json())
   .then((object) => {
